12638b0cd4f5ad6d41e0fe1b1a8c03f92b7aaba4
Louis Kleiver <louis.kleiver@gmail.com>
2020-10-03 13:35:49 +0200

    Add login activity

--- app/src/main/java/eu/kleiver/epicture/MainActivity.kt
+++ app/src/main/java/eu/kleiver/epicture/MainActivity.kt
@@ -1,11 +1,20 @@
 package eu.kleiver.epicture
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import eu.kleiver.epicture.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/eu/kleiver/epicture/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package eu.kleiver.epicture.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
