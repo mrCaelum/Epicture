a565fa6af92cd1f06f99c33b7e6da7504adb1e1b
Louis Kleiver <louis.kleiver@gmail.com>
2020-10-21 21:19:08 +0200

    [STABLE] Fix: content view missing

--- app/src/main/java/eu/kleiver/epicture/HomeActivity.kt
+++ app/src/main/java/eu/kleiver/epicture/HomeActivity.kt
@@ -9,6 +9,7 @@ import kotlinx.android.synthetic.main.activity_home.*
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
 
         val uploadFragment = UploadFragment()
         val favouritesFragment = FavouritesFragment()
