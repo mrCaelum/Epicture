4a96c8a6bf94a0f09d6b5ca2619bd5cbe9ed9326
Louis <louis.lecce@epitech.eu>
2020-10-21 18:14:37 +0200

    Add fragments, UI,...

--- app/src/main/java/eu/kleiver/epicture/HomeActivity.kt
+++ app/src/main/java/eu/kleiver/epicture/HomeActivity.kt
@@ -2,9 +2,16 @@ package eu.kleiver.epicture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuInflater
 import android.widget.ScrollView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import eu.kleiver.epicture.fragments.*
+import kotlinx.android.synthetic.main.activity_home.*
 import org.json.JSONObject
+import android.view.Menu
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +27,35 @@ class HomeActivity : AppCompatActivity() {
 
             }
         }
+
+        val uploadFragment = UploadFragment()
+        val favouritesFragment = FavouritesFragment()
+        val settingsFragment = SettingsFragment()
+        val profileFragment = ProfileFragment()
+        val searchFragment = SearchFragment()
+
+        makeCurrentFragment(profileFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.ic_upload -> makeCurrentFragment(uploadFragment)
+                R.id.ic_favorite -> makeCurrentFragment(favouritesFragment)
+                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+                R.id.ic_search -> makeCurrentFragment(searchFragment)
+                R.id.ic_settings -> makeCurrentFragment(settingsFragment)
+            }
+            true
+        }
     }
 
     override fun onResume() {
         super.onResume()
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
 }
